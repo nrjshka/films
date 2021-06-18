bc44fe90e56906690db187d6f58bca0767158741
Max Korolev <nrjshka@gmail.com>
2021-06-18 09:19:51 +0200

    Starting from the scratch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { createConnection } from 'typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FilmModule } from './film/filmd.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      useFactory: async () =>
-        await createConnection({
-          type: 'mysql',
-          host: process.env.DB_HOST,
-          port: parseInt(process.env.DB_PORT) || 3306,
-          username: process.env.DB_USER,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME,
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          synchronize: false,
-        }),
-    }),
-    FilmModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
