ed87c065608480eef3da29706c1073e74623b263
Max Korolev <nrjshka@gmail.com>
2021-06-21 22:19:45 +0200

    Added: history support to react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+
+import { appHistory } from './utils';
 
 import { App } from './components';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={appHistory}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root'),
 );
